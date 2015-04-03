cb8af57ceda8454ba7712fcd8d2a4e2dc5758a02
Alan <chromatic.world@gmail.com>
2015-04-03 19:19:19 +0200

    Remove an item from cart

--- public/js/cart/cart-controller.js
+++ public/js/cart/cart-controller.js
@@ -7,4 +7,25 @@ angular.module('ecommerce')
       //Just refresh number of products in cart
       $scope.cart = Cart.getCart();
 
+
+    $scope.removeCart = function(id){
+
+
+      var index = -1;   
+      var comArr = eval( $scope.cart );
+      for( var i = 0; i < comArr.length; i++ ) {
+        if( comArr[i].id === id ) {
+          index = i;
+          break;
+        }
+      }
+      if( index === -1 ) {
+        alert( "Something gone wrong" );
+      }
+      $scope.cart.splice( index, 1 );  
+
+      Cart.setCart($scope.cart);
+
+    };
+
     }]);
